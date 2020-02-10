e2cce1ce831c3bb3a64e8850119e2922d7de2618
NicoleKuong <nicolekuong@gmail.com>
2020-02-10 10:58:58 +0100

    add functionality in router for posting

--- image/router.js
+++ image/router.js
@@ -3,6 +3,15 @@ const Image = require("./model");
 
 const router = new Router();
 
+router.post("/images", async function(request, response, next) {
+  try {
+    const image = await Image.create(request.body);
+    response.send(image);
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.get("/images", async (req, res, next) => {
   try {
     const images = await Image.findAll();
